b7665c162e32768dd2c6c47e65f5ea9cccc62657
mngdnmw <mtdunmow@gmail.com>
2018-11-12 15:04:18 +0800

    section 9 complete

--- section_nine/src/containers/Blog/Blog.js
+++ section_nine/src/containers/Blog/Blog.js
@@ -3,19 +3,52 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from '../../axios';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => { return { ...post, author: 'me' } });
+                this.setState({ posts: updatedPosts });
+            })
+            .catch(error => this.setState({ error: true }));
+    }
+
+    postClickedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postClickedHandler(post.id)}
+                />
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
--- section_nine/src/index.js
+++ section_nine/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'Auth token';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    // edit request config - headers etc
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
