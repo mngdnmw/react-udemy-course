db243b0a9d25de5af24d39f94a1ffb995bdc9503
mngdnmw <mtdunmow@gmail.com>
2018-11-22 15:59:56 +0800

    section 12 and 13 complete

--- section_eight_burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ section_eight_burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -5,7 +5,7 @@ import classes from "./CheckoutSummary.css";
 const checkoutSummary = props => {
   return (
     <div className={classes.CheckoutSummary}>
-      <h1>We hope it tastes well!</h1>
+      <h1>This is your order</h1>
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
